978bb7a2c6fcc0fd511eec4cb9626a716ee5ee53
jbacon <julien.baccon.1@gmail.com>
2023-07-14 16:42:07 -0400

    adding main

---
+++ src/main.c
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2023 by Electronya
+ *
+ * @file      main.c
+ * @author    jbacon
+ * @date      2023-07-14
+ * @brief     DIY GT wheel Firmware
+ *
+ * This file is the entry point for the Electronya DIY GT Wheel.
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/printk.h>
+
+#define MAIN_MODULE_NAME main_module
+
+/* Setting module logging */
+LOG_MODULE_REGISTER(MAIN_MODULE_NAME);
+
+void main(void)
+{
+  int rc = 0;
+  LedCtrlBaseColor color = LED_CTRL_RED;
+
+  LOG_INF("booting gt wheel");
+}
