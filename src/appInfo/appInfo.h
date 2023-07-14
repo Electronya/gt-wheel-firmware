8446b2c0cb7e9959a51bab8fbab2b0b131f9e6bf
jbacon <julien.baccon.1@gmail.com>
2023-07-14 16:42:25 -0400

    adding app info command

---
+++ src/appInfo/appInfo.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2020 by Electronya
+ *
+ * @file      appInfo.h
+ * @author    jbacon
+ * @date      2020-10-24
+ * @brief     Application information strings
+ *
+ * @defgroup  appCmd appCmd
+ * @brief     app Command Module
+ *
+ * This module is the implementation of the app information
+ * @{
+ */
+
+#ifndef APP_INFO_H
+#define APP_INFO_H
+
+/** Application info title */
+#define APP_INFO_TITLE    "Application Information"
+
+/** Application name */
+#define APP_NAME          "Name: DIY GT Wheel"
+
+/** Application version */
+#define APP_VERSION       "Version: 1.0.0"
+
+/** app command usage */
+#define APP_CMD_USAGE     "Application related commands."
+
+/** app info command usage */
+#define APP_INFO_USAGE    "Display application information.\nUsage: app info"
+
+/** app name command usage */
+#define APP_NAME_USAGE    "Display application name.\nUsage: app name"
+
+/** app version command usage */
+#define APP_VER_UAGE      "Display application version.\nUsage: app version"
+
+#endif    /* APP_INFO_H */
+
+/** @} */
