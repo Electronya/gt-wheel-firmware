17570b6db3a6cf0d1905a44fab063c8a45dc9ad3
jbacon <julien.baccon.1@gmail.com>
2023-08-25 07:28:36 -0400

    adding hidDevice module files

---
+++ src/hidDevice/hidDevice.c
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2023 by Electronya
+ *
+ * @file      hidDevice.c
+ * @author    jbacon
+ * @date      2023-08-24
+ * @brief     HID Device Module
+ *
+ *            This file is the implementation of the HID device module.
+ *
+ * @ingroup  hidDevice
+ *
+ * @{
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
+
+#include "hidDevice.h"
+#include "zephyrCommon.h"
+#include "zephyrThread.h"
+
+#define HID_DEVICE_MODULE_NAME  hid_device_module
+
+/* Setting module logging */
+LOG_MODULE_REGISTER(HID_DEVICE_MODULE_NAME);
+
+
+/** @} */
---
+++ src/hidDevice/hidDevice.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2023 by Electronya
+ *
+ * @file      hidDevice.h
+ * @author    jbacon
+ * @date      2023-08-24
+ * @brief     HID Device Module
+ *
+ *            This file is the declaration of the HID device module.
+ *
+ * @defgroup  hidDevice
+ *
+ * @{
+ */
+
+#ifndef HID_DEVICE
+#define HID_DEVICE
+
+#endif    /* HID_DEVICE */
+
+/** @} */
