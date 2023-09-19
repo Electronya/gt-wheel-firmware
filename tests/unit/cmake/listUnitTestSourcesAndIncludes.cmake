e2d1826e815971b408ba4001a468e39eeda766cf
jbacon <julien.baccon.1@gmail.com>
2023-09-19 07:01:20 -0400

    hidDevice module creation WIP

--- tests/unit/cmake/listUnitTestSourcesAndIncludes.cmake
+++ tests/unit/cmake/listUnitTestSourcesAndIncludes.cmake
@@ -21,6 +21,12 @@ macro(getFileListForSuite sourceList includeList)
     listIncludesDir(${CMAKE_CURRENT_SOURCE_DIR}/../../src modInc)
   endif()
 
+  if(TEST_SUITE STREQUAL "hidDevice")
+    listSources(${CMAKE_CURRENT_SOURCE_DIR}/hidDevice testSrc)
+    listIncludesDir(${CMAKE_CURRENT_SOURCE_DIR}/hidDevice testInc)
+    listIncludesDir(${CMAKE_CURRENT_SOURCE_DIR}/../../src modInc)
+  endif()
+
   # message("testSrc: ${testSrc}")
   # message("testInc: ${testInc}")
   # message("modSrc: ${modSrc}")
