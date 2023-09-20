ac7c772a1aff1234d28524d59f35caeed6e4096b
jbacon <julien.baccon.1@gmail.com>
2023-09-20 19:12:04 -0400

    hidDevice WIP, still missing clutch reading

--- src/hidDevice/hidDevice.c
+++ src/hidDevice/hidDevice.c
@@ -18,17 +18,143 @@
 #include <zephyr/sys/util.h>
 
 #include "hidDevice.h"
+#include "buttonMngr.h"
 #include "zephyrCommon.h"
-#include "zephyrThread.h"
+#include "zephyrHID.h"
+#include "zephyrWork.h"
+#include "zephyrWorkQueue.h"
 
 #define HID_DEVICE_MODULE_NAME  hid_device_module
 
 /* Setting module logging */
 LOG_MODULE_REGISTER(HID_DEVICE_MODULE_NAME);
 
+/* Creating the worker stack */
+K_THREAD_STACK_DEFINE(hidWorkerStack, 128);
+
+/**
+ * @brief The HID device label.
+*/
+#define HID_DEVICE_LABEL              "HID_0"
+
+/**
+ * @brief The gamepad report.
+*/
+#define HID_GAMEPAD_REPORT            0x01
+
+/**
+ * @brief The button states report offset.
+*/
+#define HID_RPT_BTN_STATE_OFFSET      2
+
+/**
+ * @brief The HID device work queue.
+*/
+static ZephyrWorkQueue workQueue = {
+  .priority = 1,
+  .stack = hidWorkerStack,
+  .stackSize = K_THREAD_STACK_SIZEOF(hidWorkerStack),
+};
+
+/**
+ * @brief The HID device work.
+*/
+static ZephyrWork work;
+
+/**
+ * @brief The HID report (report ID + clutch value + button states).
+*/
+static uint8_t report[HID_RPT_BTN_STATE_OFFSET + BUTTON_COUNT];
+
+/**
+ * @brief HID descriptor.
+*/
+uint8_t hidDescriptor[] = {
+  HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),
+	HID_USAGE(HID_USAGE_GEN_DESKTOP_GAMEPAD),
+	HID_COLLECTION(HID_COLLECTION_APPLICATION),
+  HID_COLLECTION(HID_COLLECTION_PHYSICAL),
+	HID_REPORT_ID(HID_GAMEPAD_REPORT),
+  HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),
+  HID_USAGE(HID_USAGE_GEN_DESKTOP_X),
+  HID_LOGICAL_MIN8(0),
+	HID_LOGICAL_MAX8(255),
+	HID_REPORT_SIZE(8),
+	HID_REPORT_COUNT(1),
+	HID_INPUT(0x02),
+  HID_USAGE_PAGE(HID_USAGE_GEN_BUTTON),
+  HID_USAGE_MIN8(1),
+  HID_USAGE_MAX8(BUTTON_COUNT),
+  HID_LOGICAL_MIN8(0),
+  HID_LOGICAL_MAX8(1),
+  HID_REPORT_SIZE(8),
+  HID_REPORT_COUNT(BUTTON_COUNT),
+  HID_INPUT(0x02),
+  HID_END_COLLECTION,
+	HID_END_COLLECTION,
+};
+
+/**
+ * @brief   The HID IN interrupt operation.
+ *
+ * @param dev   The HID device.
+ */
+static void hidInIntOp(const struct device *dev)
+{
+  int rc;
+
+  rc = zephyrWorkSubmitToQueue(&workQueue, &work);
+  if(rc < 0)
+    LOG_ERR("unable to submit the HID IN interrupt work");
+  else if(rc == 0)
+    LOG_ERR("HID IN interrupt work already submitted");
+}
+
+/**
+ * @brief The HID device.
+*/
+static ZephyrHID hidDev = {
+  .ops = {
+    .int_in_ready = hidInIntOp,
+  },
+};
+
+/**
+ * @brief   The HID device worker.
+ *
+ * @param item  The work item.
+ */
+static void hidWorker(struct k_work *item)
+{
+  int rc;
+  uint8_t *repBtnStates = (WheelButtonState *)report + HID_RPT_BTN_STATE_OFFSET;
+
+  report[0] = HID_GAMEPAD_REPORT;
+
+  // TODO: read the clutch value.
+
+  rc = buttonMngrGetAllStates(repBtnStates, BUTTON_COUNT);
+  if(rc < 0)
+  {
+    LOG_ERR("unable to populate button report with button states");
+    return;
+  }
+}
+
 int hidDeviceInit(void)
 {
-  return 0;
+  int rc;
+
+  hidDev.descriptor = hidDescriptor;
+  rc = zephyrHidInit(&hidDev, HID_DEVICE_LABEL);
+  if(rc < 0)
+    return rc;
+
+  work.handler = hidWorker;
+  zephyrWorkInit(&work);
+  zephyrWorkQueueInit(&workQueue);
+
+  return rc;
 }
 
 /** @} */
--- tests/unit/hidDevice/test_hidDevice.c
+++ tests/unit/hidDevice/test_hidDevice.c
@@ -22,6 +22,7 @@
 #include "hidDevice.h"
 #include "hidDevice.c"
 
+#include "buttonMngr.h"
 #include "zephyrHID.h"
 #include "zephyrWorkQueue.h"
 #include "zephyrWork.h"
@@ -29,13 +30,20 @@
 DEFINE_FFF_GLOBALS;
 
 /* mocks */
+FAKE_VALUE_FUNC(int, zephyrHidInit, ZephyrHID*, uint8_t*);
+
+FAKE_VOID_FUNC(zephyrWorkQueueInit, ZephyrWorkQueue*);
+FAKE_VOID_FUNC(zephyrWorkInit, ZephyrWork*);
+FAKE_VALUE_FUNC(int, zephyrWorkSubmitToQueue, ZephyrWorkQueue*, ZephyrWork*);
+
+FAKE_VALUE_FUNC(int, buttonMngrGetAllStates, WheelButtonState*, size_t);
 
 /**
  * @brief The test fixture.
 */
 struct hidDevice_suite_fixture
 {
-
+  WheelButtonState testStates[BUTTON_COUNT];
 };
 
 static void *hidDeviceSuiteSetup(void)
@@ -55,12 +63,53 @@ static void hidDeviceSuiteTeardown(void *f)
 static void hidDeviceCaseSetup(void *f)
 {
   // int successRet = 0;
-  // struct hidDevice_suite_fixture *fixture =
-  //   (struct hidDevice_suite_fixture *)f;
+  struct hidDevice_suite_fixture *fixture =
+    (struct hidDevice_suite_fixture *)f;
+
+  for(uint8_t i = 0; i < BUTTON_COUNT; ++i)
+    fixture->testStates[i] = BUTTON_DEPRESSED;
+
+  RESET_FAKE(zephyrHidInit);
+  RESET_FAKE(zephyrWorkQueueInit);
+  RESET_FAKE(zephyrWorkInit);
+  RESET_FAKE(zephyrWorkSubmitToQueue);
+  RESET_FAKE(buttonMngrGetAllStates);
 }
 
 ZTEST_SUITE(hidDevice_suite, NULL, hidDeviceSuiteSetup, hidDeviceCaseSetup,
   NULL, hidDeviceSuiteTeardown);
+
+/**
+ * @test  hidInIntOp must submit the HID work.
+*/
+ZTEST(hidDevice_suite, test_hidInIntOp_SubmitWork)
+{
+  int successRet = 1;
+
+  zephyrWorkSubmitToQueue_fake.return_val = successRet;
+
+  hidInIntOp(NULL);
+  zassert_equal(1, zephyrWorkSubmitToQueue_fake.call_count);
+  zassert_equal(&workQueue, zephyrWorkSubmitToQueue_fake.arg0_val);
+  zassert_equal(&work, zephyrWorkSubmitToQueue_fake.arg1_val);
+}
+
+/**
+ * @test  hidWorker must return do nothing more if the operation to get all
+ *        the button states fails.
+*/
+ZTEST(hidDevice_suite, test_hidWorker_GetButtonStateFail)
+{
+  int failRet = -EIO;
+
+  buttonMngrGetAllStates_fake.return_val = failRet;
+
+  hidWorker(NULL);
+  zassert_equal(1, buttonMngrGetAllStates_fake.call_count);
+  zassert_equal((WheelButtonState *)&report + HID_RPT_BTN_STATE_OFFSET,
+    buttonMngrGetAllStates_fake.arg0_val);
+  zassert_equal(BUTTON_COUNT, buttonMngrGetAllStates_fake.arg1_val);
+}
 
 /**
  * @test  hidDeviceInit must return the error code if initializing the HID
@@ -68,7 +117,36 @@ ZTEST_SUITE(hidDevice_suite, NULL, hidDeviceSuiteSetup, hidDeviceCaseSetup,
 */
 ZTEST(hidDevice_suite, test_hidDeviceInit_InitFail)
 {
+  int failRet = -ENODEV;
 
+  zephyrHidInit_fake.return_val = failRet;
+
+  zassert_equal(failRet, hidDeviceInit());
+  zassert_equal(1, zephyrHidInit_fake.call_count);
+  zassert_equal(&hidDev, zephyrHidInit_fake.arg0_val);
+  zassert_equal((uint8_t *)HID_DEVICE_LABEL, zephyrHidInit_fake.arg1_val);
+}
+
+/**
+ * @test  hidDeviceInit must initialize the HID device, the work queue and the
+ *        work, and return the success code.
+*/
+ZTEST(hidDevice_suite, test_hidDeviceInit_Success)
+{
+  int successRet = 0;
+
+  zephyrHidInit_fake.return_val = successRet;
+
+  zassert_equal(successRet, hidDeviceInit());
+  zassert_equal(hidInIntOp, hidDev.ops.int_in_ready);
+  zassert_equal(1, zephyrHidInit_fake.call_count);
+  zassert_equal(&hidDev, zephyrHidInit_fake.arg0_val);
+  zassert_equal((uint8_t *)HID_DEVICE_LABEL, zephyrHidInit_fake.arg1_val);
+  zassert_equal(hidWorker, work.handler);
+  zassert_equal(1, zephyrWorkInit_fake.call_count);
+  zassert_equal(&work, zephyrWorkInit_fake.arg0_val);
+  zassert_equal(1, zephyrWorkQueueInit_fake.call_count);
+  zassert_equal(&workQueue, zephyrWorkQueueInit_fake.arg0_val);
 }
 
 /** @} */
